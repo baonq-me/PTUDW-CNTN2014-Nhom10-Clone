2bca8961ec0779699f3eb7e54d5c9864d70c40ca
1412170 <1412170@student.hcmus.edu.vn>
2017-04-28 23:28:19 +0700

    Strength Password

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
+/*Kiểm tra tính đúng đắn của form đăng ký tài khoản*/
+function validateFormSignUp(){
+	var password1 = document.forms["sign_up_form"]["sign_up_password"].value;
+	var password2 = document.forms["sign_up_form"]["sign_up_reenter_password"].value;
 
+	if(password1 != password2){
+		document.getElementById("sign_up_msg_reenter_pass").innerHTML = "Mật khẩu nhập lại không trùng khớp!!!"
+		return false;
+	}
+};
 
 $(document).ready(function(){
 	$(window).scroll(function(){
@@ -42,5 +51,71 @@ $(document).ready(function(){
     	$(".popup-login").css("display", "block");
     });
 
+
+    //Kiểm tra độ mạnh của password
+    $('#sign_up_password').keyup(function()
+	{
+		$('#result_strength_pass').html(checkStrength($('#sign_up_password').val()))
+	});
+	
+	
+	function checkStrength(password)
+	{
+		//initial strength
+		var strength = 0
+		
+		//if the password length is less than 8, return message.
+		if (password.length < 8) { 
+			$('#result_strength_pass').removeClass()
+			$('#result_strength_pass').addClass('short')
+			return 'Quá ngắn!' 
+		}
+		
+		//length is ok, lets continue.
+		
+		//if length is 8 characters or more, increase strength value
+		if (password.length > 7) strength += 1
+		
+		//if password contains both lower and uppercase characters, increase strength value
+		if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/))  strength += 1
+		
+		//if it has numbers and characters, increase strength value
+		if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/))  strength += 1 
+		
+		//if it has one special character, increase strength value
+		if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/))  strength += 1
+		
+		//if it has two special characters, increase strength value
+		if (password.match(/(.*[!,%,&,@,#,$,^,*,?,_,~].*[!,%,&,@,#,$,^,*,?,_,~])/)) strength += 1
+		
+		//now we have calculated strength value, we can return messages
+		
+		//if value is less than 2
+		if (strength < 2 )
+		{
+			$('#result_strength_pass').removeClass()
+			$('#result_strength_pass').addClass('weak')
+			return 'Yếu!'			
+		}
+		else if (strength == 2 )
+		{
+			$('#result_strength_pass').removeClass()
+			$('#result_strength_pass').addClass('good')
+			return 'Khá!'		
+		}
+		else
+		{
+			$('#result_strength_pass').removeClass()
+			$('#result_strength_pass').addClass('strong')
+			return 'Mạnh!'
+		}
+	}
+
 });
+
+
+
+
+
+
 
