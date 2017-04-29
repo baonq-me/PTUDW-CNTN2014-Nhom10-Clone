0693b22e085fbc6d3c5a85906bc66a8e244f10ce
1412209 <1412209@student.hcmus.edu.vn>
2017-04-29 22:43:00 +0700

    huy

--- js/script.js
+++ js/script.js
@@ -130,11 +130,52 @@ $(document).ready(function(){
 		}
 	}
 
+    // Set Bill info
+    setBillingInfo();
+
+    // Set Phương thức thanh toán
+	$(".cart-pay .form-pay-info .pay-info").slideUp(0);
+	$(".cart-pay input[name=pay-method]").change(function(){
+		if($(".cart-pay input[name=pay-method]:checked").val() === "by-card"){
+			$(".cart-pay .form-pay-info .pay-info").slideDown(500);
+			setNoneValFormPayInfo();
+		}
+		else{
+			$(".cart-pay .form-pay-info .pay-info").slideUp(500);
+			setDefaultFormPayInfo();
+		}
+	});
 });
+function setNoneValFormPayInfo(){
+	var form = document.forms["bill-info"];
+	form['name'].value = form['card-id'].value = form['cvv'].value 
+		= form['email'].value = form['address'].value = form['city'].value = "";
+}
+function setDefaultFormPayInfo(){
+	var form = document.forms["bill-info"];
+	form['name'].value = form['card-id'].value = form['cvv'].value 
+		= form['email'].value = form['address'].value = form['city'].value = "default";
+}
 
+function getPramJs(param) {
+	var query = window.location.search.substring(1);
+	var parms = query.split('&');
+	for (var i=0; i<parms.length; i++) {
+	var pos = parms [ i ].indexOf('=');
+	if (pos > 0) {
+	var key = parms [ i ].substring(0,pos).toLowerCase();
+	var val = parms [ i ].substring(pos+1);
+	if(key == param.toLowerCase())
+	return val;
+	}
+	}
+	return null;
+}
 
-
-
-
-
-
+function setBillingInfo(){
+	$(".cart-pay .customer-info .name i").text(getPramJs("name"));
+	$(".cart-pay .customer-info .phone i").text(getPramJs("phone"));
+	$(".cart-pay .customer-info .date i").text(decodeURIComponent(getPramJs("date")));
+	var tmp = getPramJs("base-add") + ", Quận " + getPramJs("district-add") + ", Tỉnh " + getPramJs("city-add");
+	$(".cart-pay .customer-info .address i").text(tmp);
+}
