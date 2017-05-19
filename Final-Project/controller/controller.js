bba509c1646e08a80252e1482be9791e9b8c1309
1412209 <1412209@student.hcmus.edu.vn>
2017-05-20 01:01:21 +0700

    done homepage

--- Final-Project/controller/controller.js
+++ Final-Project/controller/controller.js
@@ -1,11 +1,49 @@
 module.exports = function(app) {
 	var dao = require('../database/dao.js');
-
-	app.get("/", function(req, res){
+	// cài đặt header cơ bản
+	var setHeader = function(callback){
 		dao.getAllCategory(function(categorys){
-			dao.getNewProduct(6, function(newProducts){
-				dao.getPromotionProduct(6, function(promotionProducts){
-					res.render("index", {"categorys": categorys, "newProducts": newProducts, "promotionProducts": promotionProducts});
+			callback({"categorys": categorys});
+		});
+	}
+	// cài đặt sidebar cơ bản
+	var setSidebar = function(callback){
+		dao.getAllCategory(function(categorys){
+			callback({"categorys": categorys});
+		});
+	}
+	// Cài đặt footer cơ bản
+	var setFooter = function(callback){
+		callback({});
+	}
+	/*
+	var setSite = function(setHeader, setSidebar, setFooter, setContent, viewengine){
+		setHeader(function(header){
+			setSidebar(function(sidebar){
+				setFooter(function(footer){
+					setContentHome(function(content){
+						res.render("index", {"header": header})
+					});
+				});
+			});
+		});
+	}*/
+
+
+	var setContentHome = function(callback){
+		dao.getNewProduct(6, function(newProducts){
+			dao.getPromotionProduct(6, function(promotionProducts){
+				callback ({"newProducts": newProducts, "promotionProducts": promotionProducts});
+			});
+		});
+	}
+	app.get("/", function(req, res){
+		setHeader(function(header){
+			setSidebar(function(sidebar){
+				setFooter(function(footer){
+					setContentHome(function(content){
+						res.render("index", {"header": header, "sidebar": sidebar, "footer": footer, "content": content});
+					});
 				});
 			});
 		});
