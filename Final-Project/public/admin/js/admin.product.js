84728aff804506256344c8173c361b263499c86c
1412209 <1412209@student.hcmus.edu.vn>
2017-06-12 14:13:24 +0700

    view more admin/products

--- Final-Project/public/admin/js/admin.product.js
+++ Final-Project/public/admin/js/admin.product.js
@@ -7,51 +7,82 @@ $(document).ready(function(){
 	// 3: deleted
 	// 4: stop sell
 
-	function loadProducts(type) {
+	function loadProducts(type, count, skip, isEmpty = true) {
 		function addRow(product) {
-			var row = '<tr><td><div class="checkbox"><label><input type="checkbox" value=""></label></div></td><td><img src="' + product.imgPath + '" class="img-thumbnail"></td><td>' + product.name + '</td><td>' + product.quality + '</td><td>' + product.price + '</td><td>' + product.newPrice+ '</td><td>' + product.categorySlug+ '</td><td>' + product.dateAdded+ '</td><td>' + product.status+ '</td></tr>';
+			var row = '<tr data-name="' + product.name.toLowerCase() + '"><td><div class="checkbox"><label><input type="checkbox" value=""></label></div></td><td><img src="' + product.imgPath + '" class="img-thumbnail"></td><td>' + product.name + '</td><td>' + product.quality + '</td><td>' + product.price + '</td><td>' + product.newPrice+ '</td><td>' + product.categorySlug+ '</td><td>' + product.dateAdded+ '</td><td>' + product.status+ '</td></tr>';
 			$('#products').append(row);
 		}
+
+		// Set viewmore
+		$(".view-more").data("type", type);
 
 		$.ajax({
 			url: '/admin/api/products', //URL lay du lieu
 			type: 'GET',
 			data: {
-				type: type
+				type: type,
+				skip: skip,
+				count: count
 			},
 			success: function(res) {
-				$('#products').empty();
+				if(isEmpty)
+					$('#products').empty();
 				for (i = 0; i < res.length; ++i) {
 					addRow(res[i]);
 				}
+				if(res.length < count)
+					$(".view-more").hide()
+				else
+					$(".view-more").show()
 			}
 		});
 	}
 
 	$('#product-all').on('click', function(e) {
 		e.preventDefault();
-		loadProducts(0);
+		loadProducts(0, 10, 0);
 	});
 	
 	$('#product-stock').on('click', function(e) {
 		e.preventDefault();
-		loadProducts(1);
+		loadProducts(1, 10, 0);
 	});
 
-	$('#product-outOf').on('click', function(e) {
+	$('#product-out-of').on('click', function(e) {
 		e.preventDefault();
-		loadProducts(2);
+		loadProducts(2, 10, 0);
 	});
 
 	$('#product-deleted').on('click', function(e) {
 		e.preventDefault();
-		loadProducts(3);
+		loadProducts(3, 10, 0);
 	});
 
 	$('#product-stop-sell').on('click', function(e) {
 		e.preventDefault();
-		loadProducts(4);
+		loadProducts(4, 10, 0);
 	});
 
-	loadProducts(0);
+	$(".view-more").click(function(e){
+		e.preventDefault();
+		var type = parseInt($(this).data("type"));
+		var count = 10;
+		var skip = $("tbody#products tr").length;
+		loadProducts(type, count, skip, false);
+	});
+
+	loadProducts(0, 10, 0);
+
+	// Tìm kiếm
+	$("#product-search").keyup(function(e){
+		var q = $(this).val().toLowerCase();
+		if(q == "") $("tbody#products tr").show(200);
+		else{
+			var selectorShow = "tbody#products tr[data-name*='" + q + "']";
+			$("tbody#products tr").not(selectorShow).hide(200);
+			//alert($(selectorShow).length)
+			$(selectorShow).show(200);
+		}
+	});
+
 })
