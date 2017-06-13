ca8b8fb49ed895fe7b488d1b69840ff11df7636c
1412170 <1412170@student.hcmus.edu.vn>
2017-06-13 13:54:50 +0700

    edit bill page

--- Final-Project/public/admin/js/admin.bill.js
+++ Final-Project/public/admin/js/admin.bill.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
 								<td> ' + cartInfo + '</td> \
 								<td> ' + totalMoney + '</td> \
 								<td> ' + dateAdd + '</td> \
-								<td> ' + payMethod + ' (face-to-face gọi là COD - Cash On Delivery)</td> \
+								<td> ' + payMethod + '</td> \
 								<td> ' + status + '</td> ';
 			$('#bills').append(row);
 		}
@@ -34,11 +34,36 @@ $(document).ready(function(){
 			},
 			success: function(res) {
 				$('#bills').empty();
-				var cartStatus;
+				var status, pay_method;
 				for (i = 0; i < res.length; ++i) {
+						pay_method = res[i].billingInfo.pay_method;
+						if(pay_method === "face-to-face"){
+							pay_method = "Thanh toán khi giao hàng";
+						}
+						else if (pay_method === "by-card"){
+							pay_method = "Thanh toán qua thẻ online";
+						}
+						else if (pay_method === "by-online"){
+							pay_method = "Thanh toán qua ví điện tử";
+						}
+
+						status = res[i].status;
+						if (status.delivered == 1 && status.paid == 1){
+							status = "Đã hoàn tất";
+						}
+						else if (status.delivered == 0 && status.paid == 1){
+							status = "Đã thanh toán, Chờ giao hàng";
+						}
+						else if (status.canceled == 1){
+							status = "Đã hủy";
+						}
+						else if (status.delivered == 0 && status.paid == 0 && status.canceled == 0){
+							status = "Đang chờ giao hàng và thanh toán";
+						}
+						
 						addRow(res[i]._id, res[i].userID, res[i].receiverInfo.name, res[i].receiverInfo.phone,
 							res[i].receiverInfo.address, res[i].receiverInfo.district, res[i].receiverInfo.city,
-							res[i].cartInfo[0].productName + ' (count: ' + res[i].cartInfo[0].count + ')', res[i].totalMoney, res[i].dateAdded, res[i].billingInfo.pay_method, JSON.stringify(res[i].status));
+							res[i].cartInfo[0].productName + ' (count: ' + res[i].cartInfo[0].count + ')', res[i].totalMoney, res[i].dateAdded.substring(0, 10), pay_method, status);
 				}
 			}
 		});
