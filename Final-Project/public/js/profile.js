013548c89f370c6b308f798292fee429809b1acd
1412209 <1412209@student.hcmus.edu.vn>
2017-06-15 03:02:40 +0700

    edit profile :

--- Final-Project/public/js/profile.js
+++ Final-Project/public/js/profile.js
@@ -51,4 +51,72 @@ $(document).ready(function(){
 		loadBill(stt,4);
 		return false;
 	})
+
+	$(".profile-detail .item:not(.no-fix)").each(function(){
+		var item = $(this);
+		var form = item.find(".form");
+		if(item.find(".form").length > 0){
+			item.find(".icon-click").click(function(){
+				if(form.css("display") == "none")
+					form.css("display", "inline-block")
+				else form.css("display", "none")
+			})
+		}
+	})
+	function validateEmail(email) {
+		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(email);
+	}
+	$("#edit-email button").click(function(){
+		var email = $("#edit-email input").val();
+		if(!validateEmail(email)) return alert("Định dạng email sai");
+		$.ajax({
+			url: "/profile/update",
+			type: "post",
+			data: {
+				field: "email",
+				value: email
+			},
+			async: false,
+			success: function(result){
+				if(!result.success)
+					alert(result.status);
+				else location.reload();
+			}
+		})
+	})
+	$("#edit-phone button").click(function(){
+		var phone = $("#edit-phone input").val();
+		$.ajax({
+			url: "/profile/update",
+			type: "post",
+			data: {
+				field: "phone",
+				value: phone
+			},
+			async: false,
+			success: function(result){
+				if(!result.success)
+					alert(result.status);
+				else location.reload();
+			}
+		})
+	})
+	$("#edit-addr button").click(function(){
+		var address = $("#edit-addr input").val();
+		$.ajax({
+			url: "/profile/update",
+			type: "post",
+			data: {
+				field: "address",
+				value: address
+			},
+			async: false,
+			success: function(result){
+				if(!result.success)
+					alert(result.status);
+				else location.reload();
+			}
+		})
+	})
 })
